c906b0f20a42f57ffce82175d5f7cac4fc84eb07
aida <aidasagan369@gmail.com>
2025-03-15 12:35:33 +0500

    all files

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
     testDir: './tests',
     timeout: 30000,
+    actionTimeout: 10000, // Таймаут на каждое действие
     retries: 1,
     reporter: [['html'], ['json', { outputFile: 'test-results.json' }]],
     use: {
